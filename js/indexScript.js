6972dfc93d5fd354c5a3c87bec6feb976a10b30d
everton543 <everton.felipe.alves@gmail.com>
2020-12-26 16:37:45 -0300

    Start creating the footer of the page, Made the slider work

---
+++ js/indexScript.js
@@ -0,0 +1,45 @@
+var actualPcId = 1;
+var actualPhoneId = 4;
+var timer = setInterval(slide, 4000);
+
+function slide() {
+    var id = "slidex";
+
+//    if (screen.availWidth > screen.availHeight) {
+    id = id.replace("x", actualPcId);
+    document.getElementById(id).classList.remove("appears");
+    id = id.replace(actualPcId, actualPhoneId);
+    document.getElementById(id).classList.remove("appears");
+
+    nextImage();
+
+    id = "slidex";
+    id = id.replace("x", actualPcId);
+    document.getElementById(id).classList.add("appears");
+    id = id.replace(actualPcId, actualPhoneId);
+    document.getElementById(id).classList.add("appears");
+
+
+/*    } else {
+        id = id.replace("x", actualPhoneId);
+        document.getElementById(id).classList.remove("appears");
+        nextImage();
+        id = "slidex";
+        id = id.replace("x", actualPhoneId);
+        document.getElementById(id).classList.add("appears");
+    }*/
+}
+
+function nextImage() {
+    actualPcId += 1;
+    actualPhoneId += 1;
+
+    if (actualPcId > 3) {
+        actualPcId = 1;
+    }
+
+    if (actualPhoneId > 6) {
+        actualPhoneId = 4;
+    }
+
+}
